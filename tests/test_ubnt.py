13a297a7977629f146c6725ad8ced701ec9ad716
lena <lena.held@web.de>
2016-01-27 22:38:40 +0100

    changed test values to connect correctly

--- tests/test_ubnt.py
+++ tests/test_ubnt.py
@@ -5,14 +5,14 @@ import util.ubnt
 class MyTestCase(unittest.TestCase):
     def test_create_power_strip(self):
         global ps
-        ps = util.ubnt.Ubnt("vlanpowerstrip", 20, "192.168.1.20", 24, "ubnt", "ubnt", 6)
+        ps = util.ubnt.Ubnt("vlan13", 20, "192.168.1.20", 24, "ubnt", "ubnt", 6)
         assert isinstance(ps, util.ubnt.Ubnt)
 
         assert ps.ip == "192.168.1.20"
         assert ps.usr_name == "ubnt"
         assert ps.usr_password == "ubnt"
         assert ps.ip_mask == 24
-        assert ps.vlan_iface_name == "vlanpowerstrip"
+        assert ps.vlan_iface_name == "vlan13"
         assert ps.vlan_iface_id == 20
         assert ps.n_ports == 6
 
